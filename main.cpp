097460a6a46c5af973bb1e2608073d72265d2a71
PsychoIogical <dydoer97@gmail.com>
2021-05-27 11:32:51 -0500

    Added basic main.cpp additions

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/***********************************************
+ *
+ * Author: Dalton Yoder
+ * Date: 5/27/2021
+ * File: main.cpp
+ * Desc: "main" for the Stack implementation
+ *
+ **********************************************/
+#include "StackTester.h"
+
+int main() {        //No need for command line arguments
+    StackTester st();
+    st.run();
+
+    return 0;
+}
